1e33bdbca1f63dbdc07d0ecf47d755807f0c5d38
manahilkhalid725 <113779930+manahilkhalid725@users.noreply.github.com>
2024-12-09 22:57:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+// Get the buttons
+const staysButton = document.querySelector('.btn:nth-child(1)');
+const experiencesButton = document.querySelector('.btn:nth-child(2)');
+
+// Add event listeners for click events on both buttons
+staysButton.addEventListener('click', () => {
+    staysButton.classList.add('active');            // Add active class to Stays button
+    experiencesButton.classList.remove('active');   // Remove active class from Experiences button
+});
+
+experiencesButton.addEventListener('click', () => {
+    experiencesButton.classList.add('active');     // Add active class to Experiences button
+    staysButton.classList.remove('active');        // Remove active class from Stays button
+});
+
+
+
+const tabs = document.querySelectorAll(".scrollable-tabs-container a");
+const rightArrow = document.querySelector(
+  ".scrollable-tabs-container .right-arrow svg"
+);
+const leftArrow = document.querySelector(
+  ".scrollable-tabs-container .left-arrow svg"
+);
+const tabsList = document.querySelector(".scrollable-tabs-container ul");
+const leftArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .left-arrow"
+);
+const rightArrowContainer = document.querySelector(
+  ".scrollable-tabs-container .right-arrow"
+);
+
+const removeAllActiveClasses = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeAllActiveClasses();
+    tab.classList.add("active");
+  });
+});
+
+const manageIcons = () => {
+  if (tabsList.scrollLeft >= 20) {
+    leftArrowContainer.classList.add("active");
+  } else {
+    leftArrowContainer.classList.remove("active");
+  }
+
+  let maxScrollValue = tabsList.scrollWidth - tabsList.clientWidth - 20;
+  console.log("scroll width: ", tabsList.scrollWidth);
+  console.log("client width: ", tabsList.clientWidth);
+
+  if (tabsList.scrollLeft >= maxScrollValue) {
+    rightArrowContainer.classList.remove("active");
+  } else {
+    rightArrowContainer.classList.add("active");
+  }
+};
+
+rightArrow.addEventListener("click", () => {
+  tabsList.scrollLeft += 200;
+  manageIcons();
+});
+
+leftArrow.addEventListener("click", () => {
+  tabsList.scrollLeft -= 200;
+  manageIcons();
+});
+
+tabsList.addEventListener("scroll", manageIcons);
+
+let dragging = false;
+
+const drag = (e) => {
+  if (!dragging) return;
+  tabsList.classList.add("dragging");
+  tabsList.scrollLeft -= e.movementX;
+};
+
+tabsList.addEventListener("mousedown", () => {
+  dragging = true;
+});
+
+tabsList.addEventListener("mousemove", drag);
+
+document.addEventListener("mouseup", () => {
+  dragging = false;
+  tabsList.classList.remove("dragging");
+});
