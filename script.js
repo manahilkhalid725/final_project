567bd4f13255e6335bf979463dfec025505a4a0a
manahilkhalid725 <113779930+manahilkhalid725@users.noreply.github.com>
2024-12-20 20:54:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -91,3 +91,47 @@ document.addEventListener("mouseup", () => {
   dragging = false;
   tabsList.classList.remove("dragging");
 });
+
+
+document.querySelectorAll('.image-container i').forEach(heart => {
+  heart.addEventListener('click', function() {
+      // Toggle the 'liked' class to change the color
+      this.classList.toggle('liked');
+  });
+});
+
+
+// Select all icon buttons
+document.querySelectorAll('.icon-button').forEach(button => {
+  button.addEventListener('click', function() {
+      // Remove 'active' class from all icon buttons
+      document.querySelectorAll('.icon-button').forEach(b => b.classList.remove('active'));
+
+      // Add 'active' class to the clicked icon button
+      this.classList.add('active');
+  });
+});
+
+
+document.querySelectorAll('.box1').forEach((box) => {
+  const imageContainer = box.querySelector('.image-container');
+  const leftArrow = box.querySelector('.left-arrow2');
+  const rightArrow = box.querySelector('.right-arrow2');
+
+  const initialImage = 'https://i.pinimg.com/736x/97/7c/f5/977cf5377928ebbd518f9fb2bdd814eb.jpg';
+  const swappedImage = 'https://i.pinimg.com/736x/ea/bd/85/eabd85df09875471cbd59531231ed46c.jpg';
+
+  let currentImage = initialImage;
+
+  leftArrow.addEventListener('click', () => {
+    imageContainer.style.transition = 'background-image 0.5s ease-in-out'; // Add transition
+    currentImage = initialImage;
+    imageContainer.style.backgroundImage = `url(${currentImage})`;
+  });
+
+  rightArrow.addEventListener('click', () => {
+    imageContainer.style.transition = 'background-image 0.5s ease-in-out'; // Add transition
+    currentImage = swappedImage;
+    imageContainer.style.backgroundImage = `url(${currentImage})`;
+  });
+});
